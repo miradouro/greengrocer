3742e25a638244210fa6f9ebf2450983b215d22b
Apocalipse <RAFAELMIRADOURO@GMAIL.COM>
2022-09-24 10:45:49 -0300

    aula 7

---
+++ lib/src/auth/sign_in_screen.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+import 'components/custom_text_field.dart';
+
+class SignInScreen extends StatelessWidget {
+  const SignInScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.greenAccent,
+      body: Column(
+        children: [
+          Expanded(
+            child: Container(
+              color: Colors.greenAccent,
+            ),
+          ),
+          Expanded(
+            child: Container(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 32,
+                vertical: 40,
+              ),
+              decoration: const BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.vertical(
+                  top: Radius.circular(45),
+                ),
+              ),
+              child: Column(
+                children: const [
+                  CustomTextField(
+                    icon: Icons.email,
+                    label: 'Email',
+                  ),
+                  CustomTextField(
+                    icon: Icons.password,
+                    label: 'Senha',
+                    isSecret: true,
+                  ),
+
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
